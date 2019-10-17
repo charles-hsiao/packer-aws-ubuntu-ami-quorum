c3f5762da9fe229efda9f7b9990089adf15779d5
Charles Hsiao <charles_hsiao@maicoin.com>
2019-10-17 14:58:31 +0800

    Add monit installation config

---
+++ share/common/monit/install_monit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+M_VERSION=5.25.1
+
+# clone monit binary
+wget https://mmonit.com/monit/dist/binary/$M_VERSION/monit-$M_VERSION-linux-x64.tar.gz
+
+# unzip & copy to /usr/bin & set permission
+tar -vxzf monit-$M_VERSION-linux-x64.tar.gz
+sudo cp -f monit-$M_VERSION/bin/monit /usr/bin/monit
+sudo chmod 755 /usr/bin/monit
+
+# create config file
+sudo mkdir -p /etc/monit/conf.d
+
+# update default value permission
+sudo chmod 644 /etc/default/monit
+
+# update init config permission
+sudo chmod 755 /etc/init.d/monit
+
+# update monit config permission
+sudo chmod 600 /etc/monitrc
+
+# enable monit service
+sudo systemctl enable monit.service
+
+# clean-up 
+rm -r monit-$M_VERSION-linux-x64.tar.gz
+rm -rf monit-$M_VERSION
+
+# start monit service
+sudo service monit start
+
