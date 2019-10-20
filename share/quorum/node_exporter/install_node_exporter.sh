decfdc0fdd0bf84b2d0babac4d86d537c6c50767
Charles Hsiao <charles_hsiao@maicoin.com>
2019-10-20 15:22:48 +0800

    Fix node exporter cofig path

--- share/quorum/node_exporter/install_node_exporter.sh
+++ share/quorum/node_exporter/install_node_exporter.sh
@@ -15,8 +15,8 @@ sudo mkdir -p /etc/init.d
 # create & copy files
 mkdir -p ~/node_exporter
 sudo mv ~/node_exporter-$EXPORTER_VERSION.linux-amd64/* ~/node_exporter/
-sudo cp /tmp/node_exporter/etc__default__node_exporter /etc/default/node_exporter
-sudo cp /tmp/node_exporter/etc__init.d__node_exporter /etc/init.d/node_exporter
+sudo cp /tmp/node_exporter__etc__default__node_exporter /etc/default/node_exporter
+sudo cp /tmp/node_exporter__etc__init.d__node_exporter /etc/init.d/node_exporter
 
 # update default value permission
 sudo chmod 644 /etc/default/node_exporter
@@ -27,3 +27,4 @@ sudo chmod 755 /etc/init.d/node_exporter
 # clean-up
 rm -f node_exporter-0.18.1.linux-amd64.tar.gz
 rm -rf node_exporter-0.18.1.linux-amd64
+rm -rf /tmp/node_exporter*
