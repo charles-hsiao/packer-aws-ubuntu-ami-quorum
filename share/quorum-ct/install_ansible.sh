1ec6a284dea9820a763f883dd1f4f770fccfa60b
Charles Hsiao <skywoods917@gmail.com>
2019-10-23 00:54:34 +0800

    Update ansible installation

--- share/quorum-ct/install_ansible.sh
+++ share/quorum-ct/install_ansible.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Add apt package source
-sudo sh -c 'echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list'
+# Install Ansible from PPA
 
-# Install Ansible by apt-get
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+# apt-update
+sudo apt-get update
+
+# install common packages
+sudo apt-get install -y software-properties-common
+
+# add PPA
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+
+# install ansible
 sudo apt-get install -y ansible
